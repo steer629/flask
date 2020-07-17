eb3e811ac75fdd5ec662968622903ad25bff0c82
steer629 <36502963+steer629@users.noreply.github.com>
2020-07-17 09:47:36 +0200

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,23 +1,17 @@
 #!/bin/bash
 set -e
 
-export TRYTOND_DATABASE_URI
+: ${DB_USER:=${POSTGRES_ENV_POSTGRES_USER:='postgres'}}
+: ${DB_PASSWORD:=${POSTGRES_ENV_POSTGRES_PASSWORD}}
+: ${DB_HOSTNAME:=${POSTGRES_PORT_5432_TCP_ADDR:='postgres'}}
+: ${DB_PORT:=${POSTGRES_PORT_5432_TCP_PORT:='5432'}}
+: ${TRYTOND_DATABASE_URI:="postgresql://${DB_USER}:${DB_PASSWORD}@${DB_HOSTNAME}:${DB_PORT}/"}
+: ${PYTHONOPTIMIZE:=1}
 
-if [ "$1" = 'trytond' ]; then
-	# Listen on all interfaces
-	sed -i "/^#listen = \[::\]:8000/s/^#//" "$TRYTOND_CONFIG"
-	
-	# Run setup scripts for the server
-	if [ -d /docker-entrypoint-init.d ]; then
-		for f in /docker-entrypoint-init.d/*.sh; do
-			[ -f "$f" ] && . "$f"
-		done
-	fi
-	
-	# Reset permissions to the data directory
-	chown -R tryton:tryton "$TRYTOND_DATA"
-	
-	exec gosu tryton /usr/bin/trytond -c $TRYTOND_CONFIG
+export TRYTOND_DATABASE_URI PYTHONOPTIMIZE
+
+if [ "${1:0:1}" = '-' ]; then
+    set -- uwsgi --ini /etc/uwsgi.conf "$@"
 fi
 
 exec "$@"
